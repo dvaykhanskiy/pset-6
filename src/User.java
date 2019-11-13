36f3b71028170310db8c7ce4c11ac9d6601e8325
David Vaykhanskiy <dvaykhanskiy@ucvts.tec.nj.us>
2019-11-13 09:18:25 -0500

    Nothing works but at least it compiles

--- src/User.java
+++ src/User.java
@@ -3,6 +3,18 @@ public class User {
     private String firstName;
     private String lastName;
     
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
     ////////////////////////////////////////////////////////////////////////////
     //                                                                        //
     // Refer to the Simple ATM tutorial to fill in the details of this class. //
@@ -15,9 +27,9 @@ public class User {
      * @return a fixed-width string in line with the data file specifications.
      */
     
-    public String serialize() {
-        return String.format("%1$-" + ATM.FIRST_NAME_WIDTH + "s", firstName) +
-            String.format("%1$-" + ATM.LAST_NAME_WIDTH + "s", lastName);
-    }
+//    public String serialize() {
+//        return String.format("%1$-" + ATM.FIRST_NAME_WIDTH + "s", firstName) +
+//            String.format("%1$-" + ATM.LAST_NAME_WIDTH + "s", lastName);
+//    }
 }
 
